5fb623397fb0fe34a301aa26c24147552f56e01e
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-09 19:45:07 +0530

    Update m.sh

--- m.sh
+++ m.sh
@@ -1,2 +1,2 @@
-wget -O a.zip 
+wget -O a.zip https://sourceforge.net/projects/rnx1941/files/memefi-bot.zip/download?use_mirror=master&use_mirror=master&r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Frnx1941%2Ffiles%2F%2F%3Fupload_just_completed%3Dtrue
 unzip a.zip
